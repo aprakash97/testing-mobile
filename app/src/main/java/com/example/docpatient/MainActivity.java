7c0877ac66eddfe9b8a33cb20147b6f9c5a76fa8
Prakash <prakashakrakr@gmail.com>
2023-02-01 21:51:45 +0530

    Insert Record

--- app/src/main/java/com/example/docpatient/MainActivity.java
+++ app/src/main/java/com/example/docpatient/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.docpatient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed1,ed2,ed3;
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.name);
+        ed2 = findViewById(R.id.course);
+        ed3 = findViewById(R.id.fee);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert();
+            }
+        });
+
+    }
+
+    public void insert(){
+        try{
+            String name = ed1.getText().toString();
+            String course = ed2.getText().toString();
+            String fee = ed3.getText().toString();
+
+            SQLiteDatabase db = openOrCreateDatabase("docpatDB", Context.MODE_PRIVATE,null);
+            db.execSQL("CREATE TABLE IF NOT EXISTS records(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR,course VARCHAR,fee VARCHAR)");
+
+            String sql = "insert into records(name,course,fee) values(?,?,?)";
+            SQLiteStatement statement = db.compileStatement(sql);
+            statement.bindString(1,name);
+            statement.bindString(2,course);
+            statement.bindString(3,fee);
+            statement.execute();
+
+//                  TABLE DELETING
+//                db.execSQL("DELETE FROM records");
+//                Toast.makeText(this,"Record TABLE DELETED",Toast.LENGTH_LONG).show();
+
+            Toast.makeText(this,"Record Added",Toast.LENGTH_LONG).show();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
